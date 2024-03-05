5ddc270c31acbbd5a50fb5552beb356c594c826d
jreyess11 <josegabrielreyessolanojr@gmail.com>
2024-03-05 09:17:27 -0500

    cors i

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express, { json } from 'express'
 import { priceRouter } from './routes/priceRoutes.js' //--> !!!IMPORTANT!!! Siempre que importen un archivo extensión .js .Loquesea, siempre ponerlo en el path, ej -> './routes/template.js' --> el .js es la extensión 
+import cors from 'cors'
 
 const app = express() // --> Iniciamos express
 app.use(json()) 
 app.disable('x-powered-by') // --> Deshabilitar el header x-powered-by
 
-app.use(priceRouter)
+app.use(cors())
+app.use('/vitrina', priceRouter)
 
 const PORT = process.env.PORT || 3000 // --> Usar la variable de entorno PORT, si no usar el port 3000
 
