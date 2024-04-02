e13c3590d84d9fd7c9842b5904f4caf9a949fab2
David Cárdenas <santito500@live.com>
2024-04-02 14:24:54 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-import express, { json } from 'express'
-import { priceRouter } from './routes/priceRoutes.js' //--> !!!IMPORTANT!!! Siempre que importen un archivo extensión .js .Loquesea, siempre ponerlo en el path, ej -> './routes/template.js' --> el .js es la extensión 
-import cors from 'cors'
-import { actualizarBD } from './models/priceModels.js'
+import express, { json } from "express";
+import { priceRouter } from "./routes/priceRoutes.js"; //--> !!!IMPORTANT!!! Siempre que importen un archivo extensión .js .Loquesea, siempre ponerlo en el path, ej -> './routes/template.js' --> el .js es la extensión
+import cors from "cors";
+import { actualizarBD } from "./models/priceModels.js";
 import fs from "fs";
 import http from "http";
 import https from "https";
-import { APP_PORT } from './config.js';
+import { APP_PORT } from "./config.js";
 
 const options = {
   key: fs.readFileSync("certs/privkey.pem"),
   cert: fs.readFileSync("certs/cert.pem"),
 };
 
-const app = express() // --> Iniciamos express
-app.use(json()) 
-app.disable('x-powered-by') // --> Deshabilitar el header x-powered-by
+const app = express(); // --> Iniciamos express
+app.use(json());
+app.disable("x-powered-by"); // --> Deshabilitar el header x-powered-by
 
-app.use(cors())
-app.use('/vitrina', priceRouter)
-
+app.use(cors());
+app.use("/vitrina", priceRouter);
 
 http.createServer(app).listen(80);
 https.createServer(options, app).listen(APP_PORT);
@@ -28,10 +27,8 @@ const updateInterval = process.env.MINS_INTERVAL * 60 * 1000;
 setInterval(async () => {
   try {
     await actualizarBD.insertCards();
-    console.log('Cards updated successfully');
+    console.log("Cards updated successfully");
   } catch (err) {
-    console.error('Error updating cards:', err);
+    console.error("Error updating cards:", err);
   }
 }, updateInterval);
-
-
